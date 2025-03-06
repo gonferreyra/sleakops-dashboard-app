23b2e92df777e08a48094319a8917732e7690d9f
Gonzalo Ferreyra <gonferreyra@gmail.com>
2025-03-06 16:39:40 -0300

    change bg colors on dark mode

--- src/app/aws-pricing/loading.tsx
+++ src/app/aws-pricing/loading.tsx
@@ -1,16 +1,36 @@
+import { useColorMode } from '@chakra-ui/react';
+import { clsx } from 'clsx';
+
 export default function Loading() {
+  const { colorMode } = useColorMode();
   return (
     <div className='min-h-screen bg-secondary'>
       <div className='h-16 pt-8'>
         <main className='animate-pulse p-4 sm:p-6 lg:p-8'>
           <div className='mx-auto max-w-7xl px-6'>
-            <div className='h-10 w-2/5 rounded bg-gray-300'></div>
+            <div
+              className={clsx('h-10 w-2/5 rounded bg-gray-300', {
+                'bg-gray-700': colorMode === 'dark',
+              })}
+            ></div>
           </div>
           <div className='mt-4'>
             <div className='card space-y-4 p-6'>
-              <div className='h-64 rounded bg-gray-300'></div>
-              <div className='h-8 w-1/2 mx-auto rounded bg-gray-300'></div>
-              <div className='h-32 rounded bg-gray-300'></div>
+              <div
+                className={clsx('h-64 rounded bg-gray-300', {
+                  'bg-gray-700': colorMode === 'dark',
+                })}
+              ></div>
+              <div
+                className={clsx('h-8  mx-auto rounded bg-gray-300', {
+                  'bg-gray-700': colorMode === 'dark',
+                })}
+              ></div>
+              <div
+                className={clsx('h-32 rounded bg-gray-300', {
+                  'bg-gray-700': colorMode === 'dark',
+                })}
+              ></div>
             </div>
           </div>
         </main>
